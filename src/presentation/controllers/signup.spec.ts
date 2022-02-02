3f160c263aa9af558bb6482c5ac22f6e2a76e9b7
Duckesss <mateus.henrique223@gmail.com>
2022-02-02 20:46:06 -0300

    chore: add passWithNoTests flag on jest

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,16 @@
+import { SignUpController } from "./signup";
+
+describe("SignUp Controller", () => {
+  test("should return 400 if no name is provided", () => {
+    const sut = new SignUpController(); // sut = system under test
+    const httpRequest = {
+      body: {
+        email: "any-email",
+        password: "any-password",
+        passwordConfirmation: "any-password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+});
