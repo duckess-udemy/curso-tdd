9c899c3e85dd1523ddccd5ee9b2de49a685fcc43
Duckesss <mateus.henrique223@gmail.com>
2022-02-25 00:00:04 -0300

    feat: ensure SignUpController calls AddAccount with correct params

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,11 +6,14 @@ import {
 } from "../protocols";
 import { badRequest, serverError } from "../helpers/http-helper";
 import { InvalidParamError, MissingParamError } from "../errors";
+import { AddAccount } from "../../domain/usecases/add-account";
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
-  constructor(emailValidator: EmailValidator) {
+  private readonly addAccount: AddAccount;
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
@@ -27,7 +30,7 @@ export class SignUpController implements Controller {
         }
       }
 
-      const { email, password, passwordConfirmation } = httpRequest.body;
+      const { name, email, password, passwordConfirmation } = httpRequest.body;
 
       if (passwordConfirmation !== password) {
         return badRequest(new InvalidParamError("passwordConfirmation"));
@@ -36,6 +39,8 @@ export class SignUpController implements Controller {
       if (!isEmailValid) {
         return badRequest(new InvalidParamError("email"));
       }
+
+      this.addAccount.add({ name, email, password });
 
       return {
         statusCode: 200,
