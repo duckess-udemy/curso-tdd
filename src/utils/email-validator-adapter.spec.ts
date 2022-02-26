fcc67613dd5a689fa80e292085d020eb36734456
Duckesss <mateus.henrique223@gmail.com>
2022-02-26 17:16:52 -0300

    refactor: change email validator adapter name

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,26 +1,28 @@
-import { EmailValidatorAdapter } from "./email-validator";
+import { EmailValidatorAdapter } from "./email-validator-adapter";
 import validator from "validator";
 
 jest.mock("validator", () => ({
   isEmail: (): boolean => true,
 }));
 
+const makeSut = (): EmailValidatorAdapter => new EmailValidatorAdapter();
+
 describe("EmailValidator Adapter", () => {
   it("should return false if validator return false", () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = sut.isValid("invalid-email@mail.com");
     expect(isValid).toBe(false);
   });
 
   it("should return true if validator return true", () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isValid = sut.isValid("valid-email@mail.com");
     expect(isValid).toBe(true);
   });
 
   it("should call validator with correct email", () => {
-    const sut = new EmailValidatorAdapter();
+    const sut = makeSut();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
     sut.isValid("any@mail.com");
     expect(isEmailSpy).toHaveBeenCalledWith("any@mail.com");
