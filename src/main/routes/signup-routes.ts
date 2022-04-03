34e0153e56586171abd178dc3de6287f969c4d2c
Duckesss <mateus.henrique223@gmail.com>
2022-04-03 19:07:59 -0300

    chore: add httpCollection using rest client

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 
 export default (router: Router): void => {
   router.post("/signup", (req, res) => {
+    console.log(req.body);
     res.json({
       ok: "OK",
     });
