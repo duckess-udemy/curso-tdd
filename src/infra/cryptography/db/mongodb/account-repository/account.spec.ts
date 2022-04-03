741df1bd163597f8934a2a29c5130a5f1e15f4c4
Duckesss <mateus.henrique223@gmail.com>
2022-04-03 17:42:34 -0300

    test: ensure AccountMongoRepository returns an Account on success

---
+++ src/infra/cryptography/db/mongodb/account-repository/account.spec.ts
@@ -0,0 +1,25 @@
+import { mongoHelper } from "../helpers/mongo-helper";
+import { AccountMongoRepository } from "./account";
+
+describe("Account Mongo Repository", () => {
+  beforeAll(async () => {
+    await mongoHelper.connect();
+  });
+  afterAll(async () => {
+    await mongoHelper.disconnect();
+  });
+  it("should return an account on success", async () => {
+    const sut = new AccountMongoRepository();
+    const data = {
+      name: "valid-name",
+      email: "valid-email@mail.com",
+      password: "valid-password",
+    };
+    const account = await sut.add(data);
+    expect(account).toBeTruthy();
+    expect(account.id).toBeTruthy();
+    expect(account.name).toBe(data.name);
+    expect(account.email).toBe(data.email);
+    expect(account.password).toBe(data.password);
+  });
+});
